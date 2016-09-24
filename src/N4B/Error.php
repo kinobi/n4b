5c2d198fe8d5f162b1200306c612ec81b14b6bf5
Kinobi <kinobi@kinobiweb.com>
2016-09-24 18:24:37 +0200

    N4B\Error simplified

--- src/N4B/Error.php
+++ src/N4B/Error.php
@@ -6,8 +6,8 @@ use Exception;
 
 class Error extends Exception
 {
-    public function __construct($message = 'BB_ERROR_UNKNOWN_USER_SPECIFIED_ERROR', $code = 200, Exception $previous = null)
+    public function __construct($message = 'BB_ERROR_UNKNOWN_USER_SPECIFIED_ERROR')
     {
-        parent::__construct($message, $code, $previous);
+        parent::__construct($message);
     }
 }
